8cfa41f363995d331e95b97f1ee74202b60ba4d3
Salanta <jonathan@salanta.net>
2023-02-27 14:32:39 +1000

    update Label

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,18 +46,17 @@
         </svg>
         <div class="relative px-6 lg:px-8">
 
-            <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+            <div class="mx-auto max-w-7xl py-32 sm:py-48 lg:py-56">
                 <div class="sr-only hidden sm:mb-8 sm:flex sm:justify-center">
                     <div class="relative rounded-full py-1 px-3 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                         <a href="#" class=" font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
                     </div>
                 </div>
                 <div class="text-center">
-                    <h1 class="text-5xl font-bold tracking-tight text-gray-900 sm:text-7xl phudu">Salanta Network</h1>
-                    <p class="mt-6 text-2xl leading-8 text-gray-600">Elevating Resource Sharing</p>
+                    <h1 class="text-7xl font-bold tracking-tight text-gray-900 sm:text-7xl phudu">Boost your productivity</h1>
+                    <p class="mt-6 text-3xl leading-8 text-gray-600">Trusted by thousands of developers worldwide</p>
                     <div class=" sr-only mt-10 flex items-center justify-center gap-x-6">
                         <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-
                     </div>
                 </div>
             </div>
