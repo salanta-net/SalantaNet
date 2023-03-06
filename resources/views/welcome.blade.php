60ce0804aa2c6d9120bb0e0026a104199e02ac29
Salanta <jonathan@salanta.net>
2023-03-06 10:07:25 +1000

    Update color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,11 @@
         <div class="relative px-6 lg:px-8">
             <div class="mx-auto max-w-7xl py-32 sm:py-49 lg:py-58">
                 <div class="text-center mt-10">
-                    <h1 class="text-7xl font-bold tracking-tight text-gray-900 sm:text-7xl phudu">Boost your productivity</h1>
+                    <h1 class="text-7xl font-bold tracking-tight text-gray-900 sm:text-7xl">Boost your productivity</h1>
                     <p class="mt-6 text-3xl leading-8 text-gray-600">Trusted by thousands of developers worldwide</p>
                     <div class=" sr-only mt-10 flex items-center justify-center gap-x-6">
                         <a href="#"
-                           class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
+                           class="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">Get
                             started</a>
                     </div>
                 </div>
