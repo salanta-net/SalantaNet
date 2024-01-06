191751c589095a3255bb11fe2a61029e8a1558ab
Macbook <Jonathan@salanta.net>
2024-01-06 18:05:01 +1000

    Change design

--- resources/views/guideline.blade.php
+++ resources/views/guideline.blade.php
@@ -1,34 +1,30 @@
 <x-guest-layout>
-    <div class="">
+    <div>
         <svg class="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]" aria-hidden="true">
             <defs>
                 <pattern id="83fd4e5a-9d52-42fc-97b6-718e5d7ee527" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
                     <path d="M100 200V.5M.5 .5H200" fill="none" />
                 </pattern>
             </defs>
-            <svg x="50%" y="-1" class="overflow-visible fill-gray-50">
-                <path d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z" stroke-width="0" />
-            </svg>
+{{--            <svg x="50%" y="-1" class="overflow-visible fill-gray-50">--}}
+{{--                <path d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z" stroke-width="0" />--}}
+{{--            </svg>--}}
             <rect width="100%" height="100%" stroke-width="0" fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)" />
         </svg>
-        <div class="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-40">
+        <div class="mx-auto max-w-7xl px-6 py-0 sm:py-32 lg:flex lg:items-center lg:gap-x-10 lg:px-8 lg:py-16">
             <div class="mx-auto max-w-2xl lg:mx-0 lg:flex-auto">
-                <h1 class="mt-10 max-w-lg text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Guidelines</h1>
-                <p class="mt-6 text-lg leading-8 text-gray-600">
+                <h1 class="mt-10 max-w-lg text-4xl font-bold tracking-tight text-orange-500 sm:text-6xl">Guidelines</h1>
+                <p class="mt-6 text-lg leading-8 text-gray-100">
                     Most projects are not built or maintained by a single person. Instead, there is a collection of people involved, who all have their own personal preferences. If everyone would use their personal style, maintainability would suffer.
                 </p>
-                <p class="mt-6 text-lg leading-8 text-gray-600">
+                <p class="mt-6 text-lg leading-8 text-gray-100">
                     The members of our team regularly discuss the pros and cons of each of their individual programming preferences. When reaching a consensus, we write it down in these guidelines, together with the reasons why a certain approach was picked.
                 </p>
-                <p class="mt-6 text-lg leading-8 text-gray-600">
+                <p class="mt-6 text-lg leading-8 text-gray-100">
                     We consider our guidelines as a living document. People, teams, and opinions change over time. We don't dogmatically keep following rules we agreed on in the past, but keep challenging them. New experiences lead to new and improved guidelines.
                 </p>
-                <div class="mt-10 flex items-center gap-x-6">
-                    <a href="#" class="sr-only rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-                    <a href="#" class="sr-only text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
-                </div>
             </div>
-            <div class="mt-16 sm:mt-24 lg:mt-0 lg:flex-shrink-0 lg:flex-grow">
+            <div class="hidden lg:block mt-24 sm:mt-24 lg:mt-0 lg:flex-shrink-0 lg:flex-grow">
                 <svg viewBox="0 0 366 729" role="img" class="mx-auto w-[22.875rem] max-w-full drop-shadow-xl">
                     <title>App screenshot</title>
                     <defs>
